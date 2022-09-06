c5cfad4963444043061ac30542e9d75af07b017d
Roman <roman_boyko.48@mail.ru>
2022-09-06 11:52:22 +0300

    сделал шаблоны

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import News
+
 
 def index(request):
-    return HttpResponse('Hello world')
+    news = News.objects.all().order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(
+        request,
+        template_name='news/index.html',
+        context=context
+    )
 
 
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
+
